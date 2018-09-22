6bbe050c262dac9380a9a104eec8e3397f768151
李云鹏 <flowerinhouse@163.com>
2018-09-22 22:38:18 +0800

    Initial commit

---
+++ wrapper/bar.wrapper.js
@@ -0,0 +1,79 @@
+function BarComponent() {
+    BarModule.call(this);
+
+    this.barItemArray = [
+        {
+            title: '电视首页',
+            resourceId: 0,
+            url: 'index.html',
+            left: 0,
+            width: 132
+        },
+        {
+            title: '北岸新闻',
+            resourceId: 0,
+            url: 'news.html',
+            left: 140,
+            width: 120
+        },
+        {
+            title: '乡镇风采',
+            resourceId: 0,
+            url: 'beian.html',
+            left: 268,
+            width: 120
+        },
+        {
+            title: '部门动态',
+            resourceId: 0,
+            url: 'apartment.html',
+            left: 398,
+            width: 120
+        },
+        {
+            title: '智慧港城',
+            resourceId: 0,
+            url: 'smart.html',
+            left: 530,
+            width: 120
+        },
+        {
+            title: '文化旅游',
+            resourceId: 0,
+            url: 'culture.html',
+            left: 660,
+            width: 120
+        },
+        {
+            title: '专题专栏',
+            resourceId: 0,
+            //url: 'http://10.215.0.39/fujiandangjianNew/index2.html',
+            url: 'topic.html',
+            left: 790,
+            width: 120
+        },
+        {
+            title: '美丽莆田',
+            resourceId: 0,
+            url: 'putian.html',
+            left: 918,
+            width: 120
+        }
+        //{
+        //    title: '电视首页',
+        //    resourceId: 0,
+        //    url: '',
+        //    left: 1044,
+        //    width: 120
+        //}
+        // {
+        //     title: '电视首页',
+        //     resourceId: 0,
+        //     url: 'http://10.215.0.10:80/ui3/ui3/loading.htm?opk=4',
+        //     left: 1056,
+        //     width: 120
+        // }
+    ];
+
+
+}
